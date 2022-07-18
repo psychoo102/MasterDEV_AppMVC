b6ceaf61a6b45e43a40c25aa63ab363189c3d559
Dominik Panek <psychoo102yt@gmail.com>
2022-07-18 09:51:14 +0200

    Add validation if zip file

---
+++ MVC/Models/Validations/IsZIPFile.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models.Validations
+{
+    public class IsZIPFile : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            IFormFile file = (IFormFile)value;
+            if(value != null)
+                if (file.FileName.EndsWith(".zip"))
+                    return ValidationResult.Success;
+            return new ValidationResult("Plik musi być archiwum .zip");
+        }
+    }
+}
