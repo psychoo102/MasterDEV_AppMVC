636690a281e915beb0cb038f07c77dee27a800e2
Dominik Panek <33105114+psychoo102@users.noreply.github.com>
2022-07-19 15:16:49 +0200

    Update AppsContext.cs

--- MVC/Contexts/AppsContext.cs
+++ MVC/Contexts/AppsContext.cs
@@ -8,6 +8,6 @@ public class AppsContext : DbContext
         public DbSet<App> Apps { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql("Host=ec2-54-75-184-144.eu-west-1.compute.amazonaws.com;Database=dbaqmc80b9l532;Username=qrbientdzyfhup;Password=a6175c149104a75b9087951887a2c0b8fccfed2829ccf5c61b3d24ef98067bd1");
+        => optionsBuilder.UseNpgsql("Host=x;Database=x;Username=x;Password=x");
     }
 }
